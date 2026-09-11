01a0036dde02b6076e1deaf25f8073cd15e9a2e7
kaushal80106 <kaushal80106@gmail.com>
2026-09-12 02:28:47 +0530

    Restore admin button

--- foodon/src/Components/Nabar.jsx
+++ foodon/src/Components/Nabar.jsx
@@ -25,6 +25,7 @@ function Nabar() {
                     })
                 }
             </div>
+            <button className="menuBtn" onClick={() => window.location.href = '/admin'} style={{marginLeft: "10px", backgroundColor: "#333"}}>ADMIN</button>
             <button className="menuBtn">OUR MENU</button>
         </div>
         <div className="hamburger" onClick={()=>setshow(!show)}>
